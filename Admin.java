70cac6246e911bd1f0efaa5273308462e85de2f4
sr1042 <57067760+sr1042@users.noreply.github.com>
2020-04-15 22:13:05 +0600

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,42 @@
+public class Admin {
+    String name;
+    int id;
+    int photo;
+    String address;
+    
+    public void login() {
+        
+    }
+    
+    public void add_teacher() {
+        
+    }
+    
+    public void add_student() {
+        
+    }
+    
+    public void add_class() {
+        
+    }
+    
+    public void add_subhect() {
+        
+    }
+    
+    public void add_division() {
+        
+    }
+    
+    public void update_int() {
+        
+    }
+    
+    public void remove_student() {
+        
+    }
+    
+    public void logout() {
+        
+    }
+}
