e82eaaeb02de836bc34741c3617a38ad28316324
sr1042 <57067760+sr1042@users.noreply.github.com>
2020-04-15 22:14:47 +0600

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,34 @@
+public class Teacher {
+    String name;
+    String id;
+    int phno;
+    String address;
+    
+     public void login() {
+        
+    }
+    
+    public void mark_attendence() {
+        
+    }
+    
+    public void prepare_test_paper() {
+        
+    }
+    
+    public void conduct_exam() {
+        
+    }
+    
+    public void check_page() {
+        
+    }
+    
+    public void declare_result() {
+        
+    }
+    
+    public void logout() {
+        
+    }
+}
