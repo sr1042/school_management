8b53e46ef8ee75dfffe95aa8daab368285d7d67d
sr1042 <57067760+sr1042@users.noreply.github.com>
2020-04-15 22:13:38 +0600

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,13 @@
+public class Class {
+    int class_no;
+    int capacity;
+    char division;
+    
+    public void maintanancer() {
+        
+    }
+    
+    public void display_class() {
+        
+    }
+}
