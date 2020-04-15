978eb1f932978bf690d0f043f1af5481ca8e110a
sr1042 <57067760+sr1042@users.noreply.github.com>
2020-04-15 22:14:04 +0600

    Create Examinations.java

---
+++ Examinations.java
@@ -0,0 +1,17 @@
+public class Examinations {
+    int seat_no;
+    String exam_name;
+    String exam_date;
+    
+     public void no_of_students() {
+        
+    }
+    
+    public void subject() {
+        
+    }
+    
+    public void declare_result() {
+        
+    }
+}
