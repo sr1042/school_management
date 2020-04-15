9dcf0885abab9a8797d09756cc018bea4945b87a
sr1042 <57067760+sr1042@users.noreply.github.com>
2020-04-15 22:14:27 +0600

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,19 @@
+public class Student {
+    String name;
+    int roll_no;
+    String dob;
+    int phno;
+    String address;
+    
+     public void fill_form() {
+        
+    }
+    
+    public void pay_fees() {
+        
+    }
+    
+    public void get_enrolled() {
+        
+    }
+}
